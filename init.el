638ead1e86a432bb2b8fa62332d820ec8b119786
Steven Wisener <steven@wisener.us>
2013-06-17 13:40:46 -0400

    Added package management from purcell/emacs.d. Added better-defaults

--- init.el
+++ init.el
@@ -1,7 +1,16 @@
 (setq inhibit-startup-message t)
 
-(require 'package)
-(add-to-list 'package-archives 
-    '("marmalade" .
-      "http://marmalade-repo.org/packages/"))
-(package-initialize)
+(add-to-list 'load-path user-emacs-directory)
+
+;; Set path to dependencies
+(setq vendor-dir
+      (expand-file-name "vendor" user-emacs-directory))
+
+;; Add external projects to load path
+(dolist (project (directory-files vendor-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
+(require 'init-package)
+
+(require-package 'better-defaults)

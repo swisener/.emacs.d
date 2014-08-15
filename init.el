488ec43ed762e8f8c4dd3e5bdf445fbd2865df38
Steven Wisener <steven@wisener.us>
2014-08-15 11:28:34 -0400

    Added some convenience for ruby development. Also, added ag (the_silver_searcher) for fast searches

--- init.el
+++ init.el
@@ -12,6 +12,15 @@
 (require-package 'color-theme-solarized)
 (require-package 'magit)
 (require-package 'ace-jump-mode)
+(require-package 'ag)
+
+(require-package 'flymake-ruby)
+(add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+(require-package 'projectile)
+(projectile-global-mode)
+
+(setq ruby-deep-indent-paren nil)
 
 (load-theme 'solarized-dark t)
 

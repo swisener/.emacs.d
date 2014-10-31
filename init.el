923b1e566a7f312b93576a5ab44635d8a2614c7a
Steven Wisener <steven@wisener.us>
2014-10-31 09:22:13 -0400

    Added indentation after paste. Don't pass any options when running rspec

--- init.el
+++ init.el
@@ -18,12 +18,27 @@
 (require-package 'rspec-mode)
 (setq rspec-use-rake-when-possible nil)
 (setq rspec-spec-command "rspec")
+(setq rspec-command-options nil)
 
 (require-package 'flymake-ruby)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 (add-hook 'ruby-mode-hook (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
 
-;(electric-indent-mode t)
+(electric-indent-mode t)
+
+; Indent after yank
+(dolist (command '(yank yank-pop))
+   (eval `(defadvice ,command (after indent-region activate)
+            (and (not current-prefix-arg)
+                 (member major-mode '(emacs-lisp-mode lisp-mode
+                                                      clojure-mode    scheme-mode
+                                                      haskell-mode    ruby-mode
+                                                      rspec-mode      python-mode
+                                                      c-mode          c++-mode
+                                                      objc-mode       latex-mode
+                                                      plain-tex-mode))
+                 (let ((mark-even-if-inactive transient-mark-mode))
+                   (indent-region (region-beginning) (region-end) nil))))))
 
 (require-package 'projectile)
 (projectile-global-mode)

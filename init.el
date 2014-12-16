15b50d6e8f92ac02cf4e3da3ac58cbac114e56fb
Steven Wisener <steven@wisener.us>
2014-12-16 16:19:30 -0500

    Fixed ruby indentation

--- init.el
+++ init.el
@@ -23,6 +23,7 @@
 (require-package 'flymake-ruby)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 (add-hook 'ruby-mode-hook (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
+(setq flymake-ruby-executable "/usr/local/rbenv/shims/ruby")
 
 (electric-indent-mode t)
 
@@ -44,6 +45,21 @@
 (projectile-global-mode)
 
 (setq ruby-deep-indent-paren nil)
+;; remove this advice when we upgrade to emacs24.4, it's built in
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
 
 (load-theme 'solarized-dark t)
 

2231dc280b5ce7556163daa7dba7cf35cb7223df
Steven Wisener <steven@wisener.us>
2016-02-22 09:08:24 -0500

    Removed MELPA stable

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -3,8 +3,7 @@
 ;;; Standard package repositories
 
 ;;; Use Melpa for most packages
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.milkbox.net/packages/"))
 
 ;; If gpg cannot be found, signature checking will fail, so we
 ;; conditionally enable it according to whether gpg is available.
